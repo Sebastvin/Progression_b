5cf5b6955623989114e1da0b3a8f2063a1e83c18
Sebastvin <Sebastvin@gmail.com>
2021-09-24 12:58:26 +0200

    version 0.5

--- crypto.py
+++ crypto.py
@@ -2,53 +2,126 @@ from tkinter import *
 import re
 import requests
 from tkinter import ttk
+#import pandas as pd
+
 
 class Crypto(Frame):
     def __init__(self, name, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
 
-        self.data = requests.get('https://api.exchangerate-api.com/v4/latest/USD').json()
-        self.currencies = self.data['rates']
-
-        self.main       = Label(self, text='KRYPTOBOI', bg='grey', fg='white', relief=RAISED, borderwidth=3)
-        self.main.config(font=('Courier', 15, 'bold'))
-
-        self.code_label = Label(self, text="Type you cyptro code: ")
-        self.curr_label = Label(self, text="Type currency what you want: ")
+        font = ('Courier', 15, 'bold')
 
 
-        valid = (self.register(self.restrictNumberOnly), '%d', '%P')
-        self.crypto_code = Entry(self, bd=3, relief=RIDGE, justify=CENTER, validate='key',
-                                  validatecommand=valid)
+        self.real_curr_codes = ['USD', 'EUR', 'JPY', 'GBP', 'CHF', 'CAD', 'AUD', 'CHF', 'CNY', 'PLN']
+        self.digital_curr_codes = ['BTC', 'ETH', 'XLM', 'BNB', 'ADA', 'DOGE', 'XRP', 'LTC', 'BCH', 'DOT', 'BUSD']
 
-        self.from_currency_variable = StringVar(self)
-        self.from_currency_variable.set("USD")  # default value
-        font = ("Courier", 12, "bold")
-        self.option_add('*TCombobox*Listbox.font', font)
-        self.test = ttk.Combobox(self, textvariable=self.from_currency_variable, values=list(
-            self.currencies.keys()), font=font, state='readonly', width=15, justify=CENTER)
+
+        self.main =             Label(self, text='KRYPTOBOI', bg='grey', fg='white', relief=RAISED, borderwidth=3, font=font)
+        self.info_label_start = Label(self, text='1 NONE cost', font=('Courier', 10, 'bold'))
+        self.info_label_end =   Label(self, text='', font=('Courier', 10, 'bold'))
+
+
+        self.data =             Label(self, text="Check data", relief=SUNKEN, borderwidth=5)
+        self.price =            Label(self, text='', fg='black', bg='white', relief=RIDGE, justify=CENTER, width=17, borderwidth=3)
+
+        self.bid_price =        Label(self, text="Bid Price:", font=('Courier', 10, 'bold'))
+        self.bid_price_data =   Label(self, text='', fg='black', bg='white', relief=RIDGE, justify=CENTER, width=17,
+                                    borderwidth=3)
+        self.bid_price_end =    Label(self, text="", font=('Courier', 10, 'bold'))
+
+        self.ask_price =        Label(self, text="Ask Price:", font=('Courier', 10, 'bold'))
+        self.ask_price_data =   Label(self, text='', fg='black', bg='white', relief=RIDGE, justify=CENTER, width=17,
+                                    borderwidth=3)
+        self.ask_price_end =    Label(self, text="", font=('Courier', 10, 'bold'))
+        self.warning_label =    Label(self, text="The program uses a free API. You can check calls only 5 times per "
+                                              "minute, after this time the counter restarts. Daily limit is 500 calls.")
+
+        self.code_label =       Label(self, text="Type your cyptro code: ")
+        self.curr_label =       Label(self, text="Type currency what you want: ")
+        self.main_title =       Label(self, text="Info", bg='black', fg='white', relief=RAISED, borderwidth=3, font=font)
+
+
+
 
         self.check_button = Button(self, text="Convert", fg="black", command=self.perform)
 
-        self.main.grid(row=1, column=2, padx=10, pady=10)
-        self.code_label.grid(row=2, column=0, padx=10, pady=10)
-        self.curr_label.grid(row=3, column=0, padx=10, pady=10)
-        self.crypto_code.grid(row=2, column=1, padx=10, pady=10)
-        self.check_button.grid(row=4, column=2, padx=10, pady=10)
-        self.test.grid(row=3, column=1, padx=10, pady=10)
+        self.real_curr = StringVar(self)
+        self.real_curr.set("USD")  # default value
+
+        self.digital_curr = StringVar(self)
+        self.digital_curr.set("BTC")  # default value
+
+
+        self.real_curr_butt = ttk.Combobox(self, textvariable=self.real_curr, values=self.real_curr_codes,
+                                           state='readonly', width=15, justify=CENTER)
+
+
+        self.digital_curr_butt = ttk.Combobox(self, textvariable=self.digital_curr, values=self.digital_curr_codes,
+                                              state='readonly', width=15, justify=CENTER)
 
 
 
-    def perform(self, ):
-        code = float(self.crypto_code.get())
-        currency = self.from_currency_variable.get()
+
+        ######################################################################
+
+        self.main.grid              (row=1, column=2, padx=10, pady=10)
+
+        self.code_label.grid        (row=2, column=0, padx=10, pady=10)
+        self.curr_label.grid        (row=2, column=2, padx=10, pady=10)
+        self.real_curr_butt.grid    (row=2, column=3, padx=0, pady=10)
+        self.digital_curr_butt.grid (row=2, column=1, padx=10, pady=10)
+        self.check_button.grid      (row=2, column=4, padx=30, pady=10)
+
+        self.main_title.grid        (row=3, column=2, padx=10, pady=10)
+
+        self.data.grid              (row=4, column=2, padx=10, pady=10)
+
+        self.price.grid             (row=5, column=1, padx=0, pady=10)
+        self.info_label_start.grid  (row=5, column=0, padx=0, pady=10)
+        self.info_label_end.grid    (row=5, column=2, padx=0, pady= 10, columnspan=2)
+
+        self.bid_price.grid         (row=6, column=0, padx=10, pady=10)
+        self.bid_price_data.grid    (row=6, column=1, padx=10, pady=10)
+        self.bid_price_end.grid     (row=6, column=2, padx=0, pady=10, columnspan=2)
+
+        self.ask_price.grid         (row=7, column=0, padx=10, pady=10)
+        self.ask_price_data.grid    (row=7, column=1, padx=10, pady=10)
+        self.ask_price_end.grid     (row=7, column=2, padx=0, pady=10, columnspan=2)
+
+        self.warning_label.grid     (row=8, column=0, columnspan=4)
 
 
-        converted_amount = self.currency_converter.convert(from_curr, to_curr, amount)
-        converted_amount = round(converted_amount, 5)
-        self.converted_amount_field_label.config(text=str(converted_amount))
+    def get_info(self, real, digital):
+        url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' \
+              f'{real}&to_currency=' \
+              f'{digital}' \
+              f'&apikey=A8NNGHYPH9FAV2HO'
+        r = requests.get(url)
+        data = r.json()
+        return data['Realtime Currency Exchange Rate']
+
+
+    def perform(self,):
+        code = self.digital_curr.get()
+        currency = self.real_curr.get()
+        data = self.get_info(code, currency)
+
+
+
+        self.info_label_start.config(text=f"1 {data['2. From_Currency Name']} cost:")
+        self.info_label_end.config(text=f"{data['4. To_Currency Name']}, ({data['3. To_Currency Code']})")
+
+        self.data.config(text=f"Last data: {data['6. Last Refreshed']}")
+        self.price.config(text=str(round(float(data['5. Exchange Rate']), 2)))
+
+        self.bid_price_data.config(text=f"{round(float(data['8. Bid Price']), 2)}")
+        self.bid_price_end.config(text=f"{data['4. To_Currency Name']}, ({data['3. To_Currency Code']})")
+
+
+        self.ask_price_data.config(text=f"{round(float(data['9. Ask Price']), 2)}")
+        self.ask_price_end.config(text=f"{data['4. To_Currency Name']}, ({data['3. To_Currency Code']})")
 
     def restrictNumberOnly(self, action, string):
-        regex = re.compile(r"[0-9,]*?(\.)?[0-9,]*$")
+        regex = re.compile(r"[A-Za-z]*$")
         result = regex.match(string)
         return (string == "" or (string.count('.') <= 1 and result is not None))
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import requests
 
-
-#A8NNGHYPH9FAV2HO
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey' \
-      '=A8NNGHYPH9FAV2HO'
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=demo'
 r = requests.get(url)
 data = r.json()
 
